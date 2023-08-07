3b33d933a5ee7ac96fa592c1a807faafb5d226a2
HarrisonPaulGatt <s4008940@student.rmit.edu.au>
2023-08-08 08:46:56 +1000

    Reordered file structure to have src/

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 if not exist build/ ( mkdir build )
 robocopy .\resources .\build\resources /MIR > nul
 cd build
-g++ ../server.c ../http_response.c ../file.c -o server.exe -lws2_32
+g++ ../src/server.c ../src/http_response.c ../src/file.c -o server.exe -lws2_32
 server.exe
